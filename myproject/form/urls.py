3556294454e74d32a0b1cf59c510a7032fd10667
Sharanya-katukuri <katukurisharanya@gmail.com>
2025-11-14 16:13:17 +0530

    web

---
+++ myproject/form/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'form'
+
+urlpatterns = [
+    path('', views.signup_view, name='signup'),  # Root URL → Signup
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home_view, name='home'),
+]
